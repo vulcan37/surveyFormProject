e6c4fe16cee37ae67aead81368c07bb4539a9704
Sai Krishnna <nallurisaikrishna37@gmail.com>
2023-03-23 19:57:12 +0530

    added theme-model

---
+++ back-end/models/Theme.js
@@ -0,0 +1,39 @@
+const themeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['light', 'dark'],
+    default: 'light'
+  },
+  formType: {
+    type: String,
+    enum: ['one-one', 'single-page', 'multi-page'],
+    default: 'single-page'
+  },
+  allQuestionsMandatory: {
+    type: Boolean,
+    default: false
+  },
+  showSkipButton: {
+    type: Boolean,
+    default: true
+  },
+  optionType: {
+    type: String,
+    enum: ['radio', 'checkbox'],
+    default: 'radio'
+  },
+  font: {
+    type: String,
+    default: 'roboto'
+  },
+  color: {
+    type: String,
+    default: '#000000'
+  }
+});
+
+const Theme = mongoose.model('Theme', themeSchema);
