de11d81e97270a18168eee1d68d73c3a9042341f
Arpit Singh <arpitsingh22@hotmail.com>
2023-03-27 08:52:17 +0530

    added a jwt key and token auto expire

--- back-end/utils/generateToken.js
+++ back-end/utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken")
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_KEY)
+    return jwt.sign({ id }, process.env.JWT_KEY, {expiresIn:'7d'})
 }
 
 module.exports = generateToken
