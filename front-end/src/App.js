1fbeb71cf61339375e04d128f8b0eac280119ac1
Ajeet Kumar <kumar.ajeet889@gmail.com>
2023-03-27 21:35:05 +0530

    front-end files added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,27 @@
 import SignUp from "./Pages/SignUp/SignUp";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SignIn from "./Pages/SignIn/SignIn";
-
+import CreateSurvey from "./components/CreateSurveyList/CreateSurvey";
+import Questions from "./components/QuestionsList/Questions";
+import SurveyList from "./components/SurveyList/SurveyList";
+import UpdateSurveyList from "./components/Update Survey/UpdateSurveyList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+      <NavBar />
         <Routes>
-          <Route path="/" Component={SignIn} />
+        <Route path="/" Component={SignIn} />
           <Route path="/register" Component={SignUp} />
+          <Route path="/list" element={<SurveyList />} />
+          <Route path="/create" element={<CreateSurvey />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/update" element={<UpdateSurveyList />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ front-end/src/components/CreateSurveyList/CreateSurvey.js
@@ -0,0 +1,179 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+import "./createSurvey.css";
+
+function CreateSurvey() {
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [typeOfSurvey, setTos] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [criteria, setCriteria] = useState("");
+  const [img, setImg] = useState();
+  const navigate = useNavigate();
+
+  const surveyDataStore = async () => {
+    const data = {
+      name: name,
+      description: desc,
+      typeOfSurvey: typeOfSurvey,
+      startDate: startDate,
+      endDate: endDate,
+      criteria: criteria,
+      img: img,
+    };
+    await axios
+      .post("http://localhost:8080/api/createsurvey", data, img)
+      .then((res) => {
+        console.log(res);
+        setName("");
+        setDesc("");
+        setStartDate("");
+        setEndDate("");
+        setCriteria("");
+        setTos("");
+        swal("Good job!", `${res.data.message}`, "success");
+        setTimeout(()=>{
+          navigate("/questions");
+        },2000)
+      })
+      .catch((err) => {
+        swal("Good job!", `${err.message}`, "success");
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="container-fluid mt-3">
+      <div className="d-flex justify-content-between">
+        <h4>Create Survey</h4>
+        <div className="d-flex gap-3">
+          <button className="cancel" onClick={() => navigate("/")}>
+            Cancel
+          </button>
+          <button className="next" onClick={surveyDataStore}>
+            Next
+          </button>
+        </div>
+      </div>
+      <hr />
+      <div className="container">
+        <div className="row">
+          <div className="col-5">
+            <div className="field-container">
+              <div>
+                <label>Name</label>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  style={{ height: "50px" }}
+                  placeholder="Name here"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="field-container">
+              <div>
+                <label>Description</label>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  className="desc"
+                  placeholder="Descriptions"
+                  onChange={(e) => {
+                    setDesc(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="field-container">
+              <div>
+                <label>Type of Survey</label>
+              </div>
+              <div>
+                <select
+                  className="field-select"
+                  onChange={(e) => {
+                    setTos(e.target.value);
+                  }}
+                >
+                  <option>Select</option>
+                  <option>Visit</option>
+                  <option>Feedback</option>
+                  <option>Product</option>
+                  <option>Family</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="vl"></div>
+          </div>
+          <div className="col-5">
+            <div className="field-container">
+              <div>
+                <span>
+                  <label>Start Date</label>
+                  <input
+                    type="date"
+                    className="field-date"
+                    onChange={(e) => {
+                      setStartDate(e.target.value);
+                    }}
+                  />
+                </span>
+                <span>
+                  <label>End Date</label>
+                  <input
+                    type="date"
+                    className="field-date"
+                    onChange={(e) => {
+                      setEndDate(e.target.value);
+                    }}
+                  />
+                </span>
+              </div>
+            </div>
+            <div className="field-container">
+              <div>
+                <label>Other Criteria</label>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  style={{ height: "93px" }}
+                  placeholder="Other criteria"
+                  onChange={(e) => {
+                    setCriteria(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="field-container">
+              <div>
+                <label>Upload Image</label>
+              </div>
+              <div className="file">
+                <input
+                  type="file"
+                  placeholder="Drag and Drop to upload"
+                  onChange={(e) => {
+                    setImg(e.target.files[0].name);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateSurvey;
